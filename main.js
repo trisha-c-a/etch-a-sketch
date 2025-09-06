0a0e189923d85139d5b425b26df4f9b91b129996
trisha-c-a <trishaviking@gmail.com>
2025-09-06 15:52:19 -0500

    Add button to customize grid size

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const container = document.querySelector(".container");
 const N = 16;
-squareSize = 500/(N+1);
 
 function createGrid(N) {
   const totalSquares = N * N;
+  const squareSize = 500/(N+1);
   
   container.style.width = `${500}px`;
 
   for (let i = 0; i < totalSquares; i++) {
-    const square = document.createElement("div");
+    let square = document.createElement("div");
     square.className = "square";
     square.style.width = `${squareSize}px`;
     square.style.height = `${squareSize}px`;
@@ -32,5 +32,24 @@ function createGrid(N) {
   });
 
 }
+
+/*
+Adds functionality to a button that allows a user to customize the grid's size
+-Checks if a user has entered an grid size between 4 and 100 and if the value entered is not Null
+-Passes the grid size to createGrid() to generate the new grid
+*/
+const gridSizeButton = document.querySelector(".customGridButton");
+gridSizeButton.textContent = "Click here to change the grid size to your liking!"
+gridSizeButton.addEventListener("click",()=>{
+  let message = parseInt(prompt("To customize the grid size please pick a number between 4 and 100"));
+  
+  if (message<4||message>100||isNaN(message)){
+    alert("Please enter a valid number between 4 and 100.");
+  }
+  else{
+    container.innerHTML = ''; //Clears the existing grid
+    createGrid(message);
+  }
+})
 
 createGrid(N);
